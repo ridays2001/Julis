2be3528ed50dd61eecc280bafd81e34bef71223e
Riday <ridayshah2001@gmail.com>
2020-08-08 15:09:47 +0530

    Add: ready event

---
+++ src/listeners/ready.js
@@ -0,0 +1,40 @@
+const { Listener } = require('discord-akairo');
+const { MessageEmbed } = require('discord.js');
+
+class ReadyListener extends Listener {
+	constructor() {
+		super('ready', {
+			emitter: 'client',
+			event: 'ready'
+		});
+	}
+
+	exec() {
+		// Store the number of servers, users and channels in variables for later use.
+		const guilds = `${this.client.guilds.cache.size} servers`;
+		const users = `${this.client.users.cache.size} users`;
+		const channels = `${this.client.channels.cache.size} channels`;
+
+		// Log ready information to the console.
+		console.log(`Woke up in ${guilds}, with ${users} in ${channels}.`);
+		console.log(`I am ${this.client.user.tag}`);
+
+		// Set the bot activity.
+		this.client.user.setActivity('💙Amor Omnia Vincit❤️'); // Latin for "Love conquers everything".
+
+		/* Here, the idea is to record all timestamps whenever I wake up to plan out various commands.
+		Therefore, I send an embed with timestamp to a dedicated channel */
+
+		const readyEmbed = new MessageEmbed();
+		readyEmbed.setColor(process.env.COLOR)
+			.setAuthor(`${this.client.user.tag}`, this.client.user.displayAvatarURL())
+			.setTitle('Good Morning!')
+			.setDescription(`I am currently in **${guilds}**, with **${users}** in **${channels}**.`)
+			.setTimestamp(this.client.readyAt);
+
+		// BootLog variable stores the channel ID of a dedicated channel which I have made to record whenever I wake up.
+		return this.client.channels.cache.get(process.env.bootLog)?.send(`<@${process.env.OwnerID}>`, readyEmbed);
+	}
+}
+
+module.exports = ReadyListener;
