fcc5e5c5de50fb4d498d3e44ad646934ab6d0137
Riday <ridayshah2001@gmail.com>
2020-08-10 19:58:55 +0530

    Add: eval command

---
+++ src/commands/owner/eval.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-eval */
+
+const { Command } = require('discord-akairo');
+const { inspect } = require('util');
+const vm = require('vm');
+vm.createContext({});
+const { MessageEmbed, Message } = require('discord.js');
+
+// To use during eval;
+const { firestore } = require('../../struct/Database');
+
+class EvalCommand extends Command {
+	constructor() {
+		super('eval', {
+			aliases: ['eval', 'e'],
+			args: [
+				{
+					id: 'code',
+					match: 'content',
+					type: 'string',
+					prompt: {
+						start: message => `${message.author.username}, What would you like me to evaluate?`
+					}
+				}
+			],
+			ownerOnly: true,
+			quoted: false,
+			description: {
+				content: 'Evaluates JS code.',
+				usage: '<code>'
+			}
+		});
+	}
+
+	/**
+	 * @description - This command is used to evaluate javascript code.
+	 * @param {Message} message - The message object.
+	 * @param {Object} args - The arguments object.
+	 * @param {string} args.code - The code to evaluate.
+	 */
+	async exec(message, { code }) {
+		const embed = new MessageEmbed(); // To use during eval.
+		const token = this.client.token.split('').join('[^]{0,2}');
+		const rev = this.client.token.split('').reverse().join('[^]{0,2}');
+		const filter = new RegExp(`${token}|${rev}`, 'g');
+
+		try {
+			// Evaluate the code.
+			let output = eval(code);
+			// Resolve promises.
+			if (
+				output instanceof Promise ||
+				(Boolean(output) && typeof output.then === 'function' && typeof output.catch === 'function')
+			) {
+				output = await output;
+			}
+
+			// Inspect the output and set some basic properties.
+			output = inspect(output, { depth: 0, maxArrayLength: null });
+			// Filter the token out from the output.
+			output = output.replace(filter, '[TOKEN]');
+			// Remove back ticks and mentions.
+			output = clean(output);
+
+			return message.channel.send(`${output}`, { split: '\n', code: 'js' });
+		} catch (error) {
+			return message.channel.send(`The following error occurred \`\`\`js\n${error}\`\`\``);
+		}
+
+		/**
+		 * @description - This function escapes out back ticks ` and @ from the eval output.
+		 * @param {string} text - The text to clean
+		 * @returns {string} - Filtered text.
+		 */
+		function clean(text) {
+			return text
+				.replace(/`/g, `\`${String.fromCharCode(8203)}`)
+				.replace(/@/g, `@${String.fromCharCode(8203)}`);
+		}
+	}
+}
+
+module.exports = EvalCommand;
