b051aa88b2a4cddcf2906a1f360cce23eed25af9
Riday <ridayshah2001@gmail.com>
2020-08-13 19:20:33 +0530

    Fix: multi word args.
    - Added generator args for multi word inputs.
    - Paginated the obnoxiously long breed list.

--- src/commands/fun/dog.js
+++ src/commands/fun/dog.js
@@ -1,28 +1,17 @@
 const { Command } = require('discord-akairo');
-const { Message, MessageEmbed } = require('discord.js');
+const { Message, MessageEmbed, MessageReaction, User } = require('discord.js');
 const fetch = require('node-fetch');
 const qs = require('querystring');
 
 const { dogBreeds: breedsData } = require('../../util/data');
+const util = require('../../util/util');
+const { emoji } = require('node-emoji');
 
 class DogCommand extends Command {
 	constructor() {
 		super('dog', {
 			aliases: ['dog'],
-			args: [
-				{
-					id: 'breed',
-					match: 'option',
-					flag: ['--breed', '--b'],
-					prompt: {
-						start: 'Which breed would the dog belong to?',
-						retry: 'I didn\'t catch the breed you entered. Could you try that again?',
-						cancel: 'Okie. I will not send any dog.',
-						optional: true
-					},
-					type: 'lowercase'
-				}
-			],
+			optionFlags: ['--breed', '--b'],
 			category: 'fun',
 			description: {
 				content: 'Gets information about a dog breed.',
@@ -44,22 +33,77 @@ class DogCommand extends Command {
 
 	/**
 	 * @param {Message} message - The message object.
+	 * @returns {{breed:string}}
+	 */
+	*args(message) {
+		const flag = ['--breed', '--b'].some(e => message.content.toLowerCase().includes(e));
+		const breed = yield {
+			match: 'content',
+			flag: ['--breed', '--b'],
+			type: (_, phrase) => {
+				const input = phrase.split(' ');
+				input.splice(0, 1);
+				return input.join(' ').toLowerCase();
+			},
+			prompt: {
+				start: 'Which breed would the dog belong to?',
+				retry: 'I didn\'t catch the breed you entered. Could you try that again?',
+				cancel: 'Okie. I will not send any dog.',
+				optional: !flag
+			}
+		};
+		console.log(breed);
+		return { breed };
+	}
+
+	/**
+	 * @param {Message} message - The message object.
 	 * @param {Object} args - The args object.
 	 * @param {string} args.breed - The breed to look for.
 	 */
 	async exec(message, { breed }) {
 		let err = undefined;
 
-		/* TO - DO : Fix this! */
 		// If the breed argument is "list", send the list of available breeds.
 		if (breed === 'list') {
 			const list = breedsData.map(b => b.name);
+			const paginated = util.paginate(list, 29);
 			const embed = new MessageEmbed();
 			embed.setColor(this.client.prefColor(message.author, message.guild))
 				.setTitle('List of available breeds:')
-				.setDescription(`-${list.join('\n-')}`)
+				.setDescription(`・${paginated.items[0].join('\n・')}`)
 				.setTimestamp();
-			return message.channel.send(embed);
+
+			// Some general stuff.
+			const emojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣'];
+			const delay = () => new Promise(res => setTimeout(res, 250));
+
+			// Send a message and add reactions to it.
+			const m = await message.channel.send(embed);
+			for (const emoji of emojis) {
+				await m.react(emoji);
+				await delay();
+			}
+			try {
+				await m.awaitReactions(
+					/**
+					 * @param {MessageReaction} r - The reaction.
+					 * @param {User} u - The user.
+					 * @returns {*} - The return value is not important for us here.
+					 */
+					(r, u) => {
+						if (u.id !== message.author.id) return undefined;
+						if (!emojis.includes(r.emoji.name)) return undefined;
+						const index = emojis.indexOf(r.emoji.name);
+						embed.setDescription(`・${paginated.items[index].join('\n・')}`);
+						r.users.remove(u);
+						return m.edit('', embed);
+					}, { time: 6e4, errors: ['time'] }
+				);
+			} catch {
+				return m.reactions.removeAll();
+			}
+			return undefined;
 		}
 
 		/**
