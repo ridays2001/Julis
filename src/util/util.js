c7330faf92b23b00ee29e8dcacc2f1b3dee7cf56
Riday <ridayshah2001@gmail.com>
2020-08-13 19:20:20 +0530

    Add: paginate function

---
+++ src/util/util.js
@@ -0,0 +1,18 @@
+module.exports = {
+	/**
+	 * @description - A function to paginate long arrays into shorter ones.
+	 * @param {Array} collection - The array to paginate.
+	 * @param {number} [pageLength = 10] - The number of items per page.
+	 * @returns {{items:Array<Array>, pages:number, pageLength:number}} - The paginated data.
+	*/
+	paginate: (collection, pageLength = 10) => {
+		const pages = Math.ceil(collection.length / pageLength);
+		let startIndex = 0;
+		const items = [];
+		for (let i = 0; i < pages; i++) {
+			items.push(collection.slice(startIndex, startIndex + pageLength));
+			startIndex += pageLength;
+		}
+		return { items, pages, pageLength };
+	}
+};
