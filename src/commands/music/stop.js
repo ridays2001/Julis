8f81575217e853a23359007199261dd9cafe2286
Riday <ridayshah2001@gmail.com>
2020-09-07 17:34:13 +0530

    Add: stop command

---
+++ src/commands/music/stop.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord-akairo');
+const { Message, MessageEmbed } = require('discord.js');
+
+const { musicExit } = require('../../util/util');
+
+class StopCommand extends Command {
+	constructor() {
+		super('stop', {
+			aliases: ['stop', 'leave'],
+			category: 'music',
+			channel: 'guild',
+			description: {
+				content: 'Stop playing songs and clear the queue.',
+				/**
+				 * @description - A function to send an embed guide for the command.
+				 * @param {Message} message - The message object.
+				 * @returns {MessageEmbed} - The guide as an embed.
+				 */
+				guide: message => {
+					const guide = new MessageEmbed();
+					guide.setColor(this.client.prefColor(message.author, message.guild))
+						.setTitle(`Guide to use **${this.id}** command.`)
+						.setTimestamp();
+					return guide;
+				}
+			}
+		});
+	}
+
+	/**
+	 * @param {Message} message - The message object.
+	 * @returns {*}
+	 */
+	async exec(message) {
+		if (!message.member.voice || !message.member.voice.channel) {
+			return message.channel.send(
+				`${this.client.prefName(message)}, You need to be in the music channel to stop the music!`
+			);
+		}
+
+		await musicExit(message.guild.id, this.client);
+		return message.channel.send('Stopped playing.');
+	}
+}
+
+module.exports = StopCommand;
