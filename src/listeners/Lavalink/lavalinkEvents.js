e9bded7afe5c1dd592be21dd50b5193c1264decb
Riday <ridayshah2001@gmail.com>
2020-09-09 18:23:27 +0530

    Fix: reset votes
    - A reset is required when the track ends, or else it would be chaotic

--- src/listeners/Lavalink/lavalinkEvents.js
+++ src/listeners/Lavalink/lavalinkEvents.js
@@ -18,6 +18,9 @@ class LavalinkEventsListener extends Listener {
 		// Log basic info of all events.
 		logger.lavalink(`${type} :: ${reason}`);
 
+		// Reset the votes to prevent chaos.
+		if (type === 'TrackEndEvent') this.client.musica.votes.delete(data.guildId);
+
 		// If a track ends with finished, it means that the song ended. Time to send out music feeds.
 		if (type === 'TrackEndEvent' && reason === 'FINISHED') return musicFeed(data, this.client);
 
